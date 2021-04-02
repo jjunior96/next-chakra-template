6dc2f354025e7052394f2620d1e1d80b7ab616bc
Junior Alves <jjalves96@gmail.com>
2021-04-02 06:22:01 -0300

    feat: create _app with chakra configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import GlobalStyles from 'styles/global';
-import theme from 'styles/theme';
+import { theme } from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <Head>
         <title>App Name</title>
         <link rel="shorcut icon" href="/img/bg.png" />
@@ -16,8 +15,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="App Name" />
       </Head>
       <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+    </ChakraProvider>
   );
 }
 
